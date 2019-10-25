d67284c201e753cc33745244b5bdb77f15f5a431
shinoblackss <ejmagbriel97@gmail.com>
2019-10-25 10:02:57 -0400

    change to production

--- resources/js/components/Utility.js
+++ resources/js/components/Utility.js
@@ -1,3 +1,4 @@
-const ip = 'http://192.168.1.50:8000';
+// const ip = 'http://192.168.1.50:8000';
+const ip = 'https://gabowalletapplication.herokuapp.com/'
 
 export default ip
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Gabriel Wallet</title>
 
     <link rel="stylesheet" href="css/app.css">
 
